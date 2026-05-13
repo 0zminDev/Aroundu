017f497854ac13bb3d1819a2f5a088c7f214a7da
0zminDev <0zminDev@pm.me>
2026-05-13 20:13:16 +0200

    Added basic builder for aspire

--- Aroundu.AppHost/AppHost.cs
+++ Aroundu.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
+using Aroundu.AppHost.Model.Enums;
+using Aroundu.AppHost.Helpers;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var systemBuilder = new SystemBuilder(builder);
+
 
 builder.Build().Run();
---
+++ Aroundu.AppHost/Model/SystemEnvironment.cs
@@ -0,0 +1,10 @@
+namespace Aroundu.AppHost.Model.Enums;
+
+public enum SystemEnvironment
+{
+    Development,
+    Test,
+    TestPersistant,
+    Release,
+    Staging
+}
