3a622804fe6ba0a8643534a9a21963fe2e519948
0zminDev <0zminDev@pm.me>
2026-05-14 00:03:01 +0200

    CPM and CBM implementation

--- Aroundu.AppHost/AppHost.cs
+++ Aroundu.AppHost/AppHost.cs
@@ -1,9 +1,8 @@
-using Aroundu.AppHost.Model.Enums;
 using Aroundu.AppHost.Helpers;
 
 var builder = DistributedApplication.CreateBuilder(args);
-
 var systemBuilder = new SystemBuilder(builder);
 
+builder.AddProject<Projects.Aroundu_ApiGateway>("aroundu_api_gateway");
 
 builder.Build().Run();
